8445edfd0ea607cac89e9200b6ce9b1b87155784
Ky7az <8583937+Ky7az@users.noreply.github.com>
2022-01-01 21:11:37 +0100

    Responsive Grid

--- src/components/SoupArticleList.vue
+++ src/components/SoupArticleList.vue
@@ -6,15 +6,18 @@
             </b-col>
         </b-row>
         <SoupArticleSearch v-on:updated-search="onUpdatedSearch"/>
-        <b-row cols="6">
+        <b-row cols="1" cols-sm="2" cols-md="3" cols-lg="4" cols-xl="6">
             <b-col v-for="article in articles"
                    v-bind:key="article.id"
-                   v-bind:title="article.name">
-                <b-card v-bind:header="article.name" bg-variant="dark" text-variant="white" class="mb-4">
+                   v-bind:title="article.name"
+                   class="mb-4">
+                <b-card v-bind:header="article.name" 
+                        bg-variant="dark" 
+                        text-variant="white">
                     <b-card-text>
                         <small v-for="tag in article.tags" 
-                               v-bind:key="tag.id" 
-                               v-bind:title="tag.name">
+                                v-bind:key="tag.id" 
+                                v-bind:title="tag.name">
                             #{{tag.name}}
                         </small>
                         <router-link :to="`/soup/art/${article.slug}`">
