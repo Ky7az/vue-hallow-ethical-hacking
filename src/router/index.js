36eb5cd4f637b377c2b8e3a2fd37470b2ba5e5cc
Ky7az <8583937+Ky7az@users.noreply.github.com>
2023-04-16 11:51:25 +0200

    Hallow Home

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '@/views/Home.vue'
 import Soup from '@/views/Soup.vue'
 import Watch from '@/views/Watch.vue'
 import Pentest from '@/views/Pentest.vue'
@@ -22,6 +23,7 @@ const routes = [
     {
         path: '/',
         name: 'home',
+        component: Home,
         meta: {
             title: 'Hallow'
         }
